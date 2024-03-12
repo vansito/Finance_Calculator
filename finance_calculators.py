3ab7cababee0461e98d9325b46e587f6ce599988
Evans Foli Quaye <evan91@hotmail.it>
2024-03-12 09:23:00 +0000

    Made code cleaner using functions

--- finance_calculators.py
+++ finance_calculators.py
@@ -1,85 +1,71 @@
 import math
 
-# PSEUDOCODE:
+# Functions
 
-# print the investment string using variable investment
-# print the bond string using the variable bnd
-# ask user to choose between investment and bond from the above menu
-# if user inputs investment:
-    # ask user to input deposit
-    # ask user to input rate and divide the rate by 100
-    # ask user how many years to invest
-    # ask user to choose between simple or compound interest
-    # if user inputs simple:
-        # calculate using the 'simple' formula provided and print the total amount
-    # else if user inputs compound:
-        # calculate using the 'compound' formula provided and print the total amount
-# else if user inputs bond:
-    # ask user to enter the value of the house
-    # ask user to enter the interest rate, divide the rate by 100 and again by 12
-    # ask the user how many months their planning to repay the house
-    # calculate using the 'bond' formula provided and print the total amount
-# else:
-    # print an error message if user's enter a wrong input
+def inv_calculations():
+    """
+    a function that calculates two diffrent interest
+    simple and compound
+    """
+    deposit = int(input("\nHow much would you like to deposit?\t"))
+
+    rate = int(input("\nPlease enter the interest rate:\t")) / 100
+
+    years_plan = int(input("\nHow many years are you planning to invest?\t"))
+
+    answer = True
+    while answer:
+
+        interest = input("\nPlease choose between 'simple' or 'compound' interest:\t").lower()
+
+        if interest == "simple":
+            s_interest = int(deposit * (1 + rate * years_plan)) # Formula
+            print(f"\nYour 'simple' interest calculaton is: {s_interest}")
+            answer = False
+
+        elif interest == "compound":
+            c_interest = int(deposit * math.pow((1 + rate),years_plan)) # Formula
+            print(f"\nYour 'compound' interest calculation is: {c_interest}")
+            answer = False
+        
+        else:
+            print("Input not recognized, please try again.")
 
 
+def bond_calculations():
+    """
+    a function for calculatin the
+    repayment bond of a house
+    """
+    house_value = int(input("\nPlease enter the present value of the property:\t"))
 
-print("investment - to calculate the amount of interest you'll ear on your investment")
-print("bond \t - to calculate the amount you'll have to pay on a home loan")
-print()
+    rate = (int(input("\nPlease enter the interest rate:\t")) / 100) / 12
 
-investment = input("Please enter 'investment' or 'bond' from the menu above: ")
-investment = investment.lower()
-
-print()
-
-# This is the if statement asking the details from the user
-
-if investment == "investment":
-    deposit = int(input("How much would you like to deposit?\t"))
-
-    print()
-
-    rate = int(input("Please enter the interest rate: ")) / 100
-
-    print()
-
-    years_plan = int(input("How many years are you planning to invest?\t"))
-    print()
-
-    interest = input("Please choose between 'simple' or 'compound' interest:\t")
-    interest = interest.lower()
-    print()
-
-# This is the if statement between simple and compound interest
-
-    if interest == "simple":    
-        s_interest = int(deposit * (1 + rate * years_plan))
-        print(f"Your 'simple' interest calculaton is: {s_interest}")
-
-    elif interest == "compound":
-        c_interest = int(deposit * math.pow((1 + rate),years_plan))
-        print(f"Your 'compound' interest calculation is: {c_interest}")
-
-# this is the else if statement for the bond calculator
-
-elif investment == "bond":
-    house_value = int(input("Please enter the present value of the property:\t"))
-
-    print()
-
-    rate = (int(input("Please enter the interest rate:\t")) / 100) / 12
-
-    print()
-
-    months_plan = int(input("Please specify how many months you are planning to repay:\t"))
-
-    print()
+    months_plan = int(input("\nPlease specify how many months you are planning to repay:\t"))
 
     monthly_rpmnt = int((rate * house_value) / (1 - (1 + rate)**(- months_plan)))
-    print(f"This is the amount you have to repay each months: {monthly_rpmnt}")
 
-# This is the error message in case of invalid inpup
+    print(f"\nThis is the amount you have to repay each months: {monthly_rpmnt}")
 
-else:
-    print("Sorry this is an invalid input!")
+
+print("\nINVESTMENT - to calculate the amount of interest you'll ear on your investment")
+print("BOND \t   - to calculate the amount you'll have to pay on a home loan")
+print()
+
+while True:
+
+    investment = input("Please enter 'investment' or 'bond' from the menu above: ").lower()
+
+    if investment == "investment":
+
+        inv_calculations()
+        break
+
+
+    elif investment == "bond":
+
+        bond_calculations()
+        break
+
+    else:
+        print("Invalid input, please enter 'investment' or 'bond'")
